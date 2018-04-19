e3b060680d15fd6616a4b92f8a4642aa7ad6e0a4
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-04-19 14:25:57 +0200

    Add location LED operations
    These operations are triggered by identify menu actions in the UI.

---
+++ spec/models/manageiq/providers/redfish/physical_infra_manager/operations/led_spec.rb
@@ -0,0 +1,24 @@
+describe ManageIQ::Providers::Redfish::PhysicalInfraManager do
+  let(:server) { FactoryGirl.create(:redfish_physical_server, :vcr) }
+  subject(:ems) do
+    FactoryGirl.create(:ems_redfish_physical_infra, :vcr)
+  end
+
+  describe "#blink_loc_led", :vcr do
+    it "makes location LED start blinking" do
+      ems.blink_loc_led(server, nil)
+    end
+  end
+
+  describe "#turn_on_loc_led", :vcr do
+    it "turns on location LED" do
+      ems.turn_on_loc_led(server, nil)
+    end
+  end
+
+  describe "#turn_off_loc_led", :vcr do
+    it "turns off location LED" do
+      ems.turn_off_loc_led(server, nil)
+    end
+  end
+end
