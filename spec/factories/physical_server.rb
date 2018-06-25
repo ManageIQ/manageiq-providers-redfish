a96e3595d8fcb0187a7cfb663d8a580ed80564cf
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-06-25 21:04:47 +0200

    Lazy load PhysicalServer class in spec factory
    It seems that FactoryGirl can get into troubles when combined with
    Rails' autoloading of classes, which manifests itself with a warning
    similar to this: "toplevel constant SomeClassName referenced ...".
    
    To bypass this problem, we force FactoryGirl to lazy load the class by
    passing in a string instead of the actual class.

--- spec/factories/physical_server.rb
+++ spec/factories/physical_server.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :redfish_physical_server,
-          :class  => ManageIQ::Providers::Redfish::PhysicalInfraManager::PhysicalServer,
+          :class  => "ManageIQ::Providers::Redfish::PhysicalInfraManager::PhysicalServer",
           :parent => :physical_server do
     trait :vcr do
       ems_ref "/redfish/v1/Systems/System.Embedded.1"
