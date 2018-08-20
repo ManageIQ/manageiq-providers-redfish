c03aa7b6987e094ec5a98f5d043fd66e7a3fa5c1
Milan Zazrivec <mzazrivec@redhat.com>
2018-08-20 15:43:31 +0200

    Add plugin display name

--- lib/manageiq/providers/redfish/engine.rb
+++ lib/manageiq/providers/redfish/engine.rb
@@ -20,6 +20,10 @@ module ManageIQ
         # config.autoload_paths << root.join("app", "models").to_s
         # config.autoload_paths << root.join("lib", "my_provider").to_s
         # config.autoload_paths << Rails.root.join("app", "models", "aliases").to_s
+
+        def self.plugin_name
+          _('Redfish Provider')
+        end
       end
     end
   end
