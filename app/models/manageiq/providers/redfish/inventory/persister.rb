3b17ad4d797119ae35c94181715db6b24a11018b
Adam Grare <agrare@redhat.com>
2019-01-04 15:26:09 -0500

    Remove duplicated persister shared_options
    The shared_options were duplicated from the base persister

--- app/models/manageiq/providers/redfish/inventory/persister.rb
+++ app/models/manageiq/providers/redfish/inventory/persister.rb
@@ -1,27 +1,5 @@
 module ManageIQ::Providers::Redfish
   class Inventory::Persister < ManageIQ::Providers::Inventory::Persister
     require_nested :PhysicalInfraManager
-
-    protected
-
-    def targeted?
-      false
-    end
-
-    def strategy
-      nil
-    end
-
-    def parent
-      manager.presence
-    end
-
-    def shared_options
-      {
-        :targeted => targeted?,
-        :strategy => nil,
-        :parent   => parent
-      }
-    end
   end
 end
