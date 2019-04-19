87b5d56090d37cac90443027ed0263ee58f04f13
Miha Pleško <miha.plesko@xlab.si>
2019-04-19 09:31:56 +0200

    Enable `with_provider_object` on PhysicalServer
    With this commit we realize provider_object method on PhysicalServer
    to enable `with_provider_object` utility. Then we provide VCR test for
    it; and update some obsolete VCR cassettes down the way (they were using
    incorrect IDs from the mock server).
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- app/models/manageiq/providers/redfish/physical_infra_manager/physical_server.rb
+++ app/models/manageiq/providers/redfish/physical_infra_manager/physical_server.rb
@@ -3,5 +3,9 @@ module ManageIQ::Providers::Redfish
     def self.display_name(number = 1)
       n_('Physical Server (Redfish)', 'Physical Servers (Redfish)', number)
     end
+
+    def provider_object(connection)
+      connection.find(ems_ref)
+    end
   end
 end
